67769b35f932767932b626a85887c31c79bc8d56
goohw593 <goohw593@gmail.com>
2026-07-12 21:21:36 +0900

    재방문 웹푸시: OneSignal v16 서비스워커+로더(layout head, appId 대기·무동작)

--- build.js
+++ build.js
@@ -611,6 +611,7 @@ ${alts}
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE}" crossorigin="anonymous"></script>
 ${opts.jsonld || ''}
 <style>${CSS}</style>
+<script>window.__onesignalAppId="PASTE_ONESIGNAL_APP_ID";(function(){var id=window.__onesignalAppId;if(!id||id.indexOf("PASTE")===0)return;if(location.protocol!=="https:")return;var s=document.createElement("script");s.src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js";s.defer=true;document.head.appendChild(s);window.OneSignalDeferred=window.OneSignalDeferred||[];window.OneSignalDeferred.push(function(OneSignal){OneSignal.init({appId:id});});})();</script>
 </head>
 <body>
 <header><div class="wrap">
